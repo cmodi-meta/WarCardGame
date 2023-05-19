942f8fb25393842552fbe0620dd22f1e7989ada9
cmodi-meta <cmodi@fb.com>
2023-05-19 10:55:16 -0700

    initial commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by cmodi on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
